f48c863b45077d3fb3b94f81f25c1c8b3d0b5295
Phojiii <qasimbilal19@gmail.com>
2025-05-03 03:05:04 +0500

    Ads scripts

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
+import Script from 'next/script';
 import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      {/* Google AdSense Script */}
+      <Script
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4624388385890799"
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+      />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 }
